56b25f4a881c398e5e456453a6cf001b637e4b56
Nguyen Duy Thanh <thanh226199@student.nctu.edu.vn>
2025-10-21 13:24:33 +0700

    Upload backend source code

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,4 @@
+
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
@@ -6,3 +7,29 @@ const userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("User", userSchema);
+// // routes/user.js
+// const express = require('express');
+// const router = express.Router();
+
+// const userController = require('../controllers/userController');
+
+// // GET /users  (note: since server uses app.use('/users', userRoutes), here path is '/')
+// router.get('/', userController.getUsers);
+
+// // POST /users
+// router.post('/', userController.createUser);
+
+// module.exports = router;
+// routes/user.js
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+
+// GET /users
+router.get("/", userController.getUsers);
+
+// POST /users
+router.post("/", userController.createUser);
+
+module.exports = router;
+
